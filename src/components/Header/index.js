626cf7170e94764c3334bc42440779679c96d0ff
Ricardo Dias <ricardodias.dev@gmail.com>
2023-03-04 18:27:24 -0300

    Criando e estilizando o Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import styles from "./styles";
+
+export default function Header(props) {
+  const {title} = props;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        marginTop: 30,
+        backgroundColor: '#6ca2f7',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text:{
+        fontSize: 50,
+        color: '#FFF',
+
+    }
+})
+  
+export default styles;
